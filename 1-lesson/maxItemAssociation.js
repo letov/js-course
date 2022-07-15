9e6289a648f41958d8ed8850fdd50f6b76b6bbc7
Руслан Карымов <ruslan@eljur.team>
2022-07-15 13:20:53 +0300

    <1-lesson> fix some bug

--- 1-lesson/maxItemAssociation.js
+++ 1-lesson/maxItemAssociation.js
@@ -3,13 +3,15 @@ function maxItemAssociation(history) {
     let listIntersection = (list1, list2) => list1.filter( x => list2.includes(x) );
     let i = 0;
     while (i < history.length) {
-        let j = i + 1;
+        let j = 0;
         let concatList = history[i];
         while (j < history.length) {
-            let intersection = listIntersection(history[i], history[j]);
-            if (intersection.length > 0) {
-                concatList = concatList.concat(history[j]);
-                concatList = [...new Set(concatList)].sort();
+            if (i !== j) {
+                let intersection = listIntersection(history[i], history[j]);
+                if (intersection.length > 0) {
+                    concatList = concatList.concat(history[j]);
+                    concatList = [...new Set(concatList)].sort();
+                }
             }
             j++;
         }
