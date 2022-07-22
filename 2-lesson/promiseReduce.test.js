a97eb8f7eaaa84a57a255bf4718d09cb40e01e34
Руслан Карымов <ruslan@eljur.team>
2022-07-22 09:46:54 +0300

    <2-lesson> fix: test

--- 2-lesson/promiseReduce.test.js
+++ 2-lesson/promiseReduce.test.js
@@ -1,13 +1,15 @@
 import { test, expect, vi } from "vitest";
 import { promiseReduce } from "promiseReduce";
 
-test("", () => {
-    expect(promiseReduce(
-        [
-            () => Promise.resolve(1),
-            () => new Promise(resolve => setTimeout(() => resolve(2), 1000))
-        ],
-        (memo, value) => memo * value,
-        1)
-    ).rejects.toBe(2);
+test("", async () => {
+     expect(
+         await promiseReduce(
+            [
+                () => Promise.resolve(1),
+                () => new Promise(resolve => setTimeout(() => resolve(2), 1000))
+            ],
+            (memo, value) => memo * value,
+            1
+        )
+    ).toBe(2);
 });
